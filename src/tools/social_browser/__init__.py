edac28bfa6cfc085f158221b62c3acbc623a4351
BPhucKHMT <23521208@gm.uit.edu.vn>
2026-08-31 14:26:26 +0700

    feat(social): add manifest and action policy

---
+++ src/tools/social_browser/__init__.py
@@ -0,0 +1,19 @@
+"""Prepare social posts for human publication."""
+
+from tools.social_browser.contracts import (
+    BrowserObservation,
+    BrowserOperation,
+    PreparationResult,
+    RunStatus,
+    SocialActionManifest,
+    create_manifest,
+)
+
+__all__ = [
+    "BrowserObservation",
+    "BrowserOperation",
+    "PreparationResult",
+    "RunStatus",
+    "SocialActionManifest",
+    "create_manifest",
+]
